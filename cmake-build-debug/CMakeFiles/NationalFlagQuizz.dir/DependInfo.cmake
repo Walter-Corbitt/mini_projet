092c54c3859ba1e79a34463a327fdd0bc521b113
eric <eri.thomas@gmail.com>
2020-03-30 09:30:51 +0200

    v1

---
+++ cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eric/CLionProjects/NationalFlagQuizz/Fenetre.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/Fenetre.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/NationalFlagQuizz_autogen/mocs_compilation.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/NationalFlagQuizz_autogen/mocs_compilation.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/control/FenetreControl.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/control/FenetreControl.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/control/LoaderProgress.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/control/LoaderProgress.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FenetrePrincipale.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FenetrePrincipale.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameImage.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameImage.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameMenu.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameMenu.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameQuizz.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizz.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameQuizzTypeFlag.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizzTypeFlag.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameQuizzTypeQuestion.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizzTypeQuestion.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/frames/FrameSettings.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/frames/FrameSettings.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/main.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/main.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/model/Datas.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/model/Datas.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/rest/ClientRest.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/rest/ClientRest.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/settings/settings.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/settings/settings.cpp.o"
+  "/home/eric/CLionProjects/NationalFlagQuizz/utils/Utils.cpp" "/home/eric/CLionProjects/NationalFlagQuizz/cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/utils/Utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "NationalFlagQuizz_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/NationalFlagQuizz.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NationalFlagQuizz_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/NationalFlagQuizz_autogen.dir/ParseCache.txt"
+  "NationalFlagQuizz_autogen"
+  "CMakeFiles/NationalFlagQuizz.dir/Fenetre.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/NationalFlagQuizz_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/control/FenetreControl.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/control/LoaderProgress.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FenetrePrincipale.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameImage.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameMenu.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizz.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizzTypeFlag.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameQuizzTypeQuestion.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/frames/FrameSettings.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/main.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/model/Datas.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/rest/ClientRest.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/settings/settings.cpp.o"
+  "CMakeFiles/NationalFlagQuizz.dir/utils/Utils.cpp.o"
+  "NationalFlagQuizz"
+  "NationalFlagQuizz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NationalFlagQuizz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
