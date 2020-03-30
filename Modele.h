00fd3e4df1883a398ff8d53245087a7f28f70c1e
Darja38 <jasonlestage@orange.fr>
2020-03-30 10:11:43 +0200

    Modele 2 test

--- Modele.h
+++ Modele.h
@@ -5,7 +5,9 @@
 #ifndef MINI_PROJET_MODELE_H
 #define MINI_PROJET_MODELE_H
 #include <string>
-
+#include <map>
+#include <QtCore/QString>
+using namespace std;
 
 class Modele {
     Modele();
@@ -13,14 +15,16 @@ class Modele {
     class drapeau(Data);
     class Nom_Pays(Data);
 
-    virtual Qmap <Qstring, Nom_Pays> Test_Nom_Pays;
-    virtual Qmap <Qstring, drapeau> Test_Drapeau;
+    static map<QString,QString> codetopays;  //correspondance codepays -> nompays
+    static map<QString,QString> paystocode; //correspondance nompays->codepays
+    static map<int ,QString> listcode; //plus pratique pour les tirages aléatoires
+    static int loadprogress; //barre de progression
+    static map<QString,QImage> listeimage; //Les immages sont préchargées dans cette map
+    static vector<QString> drapeauxquizz; //l'ensemble des drapeaux du quizz
 
-
-    Qmap <Qstring, Qstring> Data;
-    void Récupération_Data();
+    void Recuperation_Data();
     void Stockage_Pays();
-
+    
 };
 
 
